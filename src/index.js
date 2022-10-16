c9239a60931e47200293cbaa1253fa5c390d1ac6
Adrien <adrien.biz@gmail.com>
2022-10-16 12:56:13 +0400

    Add background image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
+import Background from './bg.jpg';
+
+function component() {
+    const element = document.createElement('div');
+
+    const myBackground = new Image();
+    myBackground.src = Background;
+    element.appendChild(myBackground);
+
+    return element;
+}
+
+
+// document.body.appendChild(component());
 
 console.log('Working!');
