f85f76245b97b44245f0a8a39680c119ef26c2bb
Adrien <adrien.biz@gmail.com>
2022-10-19 21:19:52 +0400

    Separate header module and index.js

--- main.js
+++ main.js
@@ -573,6 +573,79 @@ module.exports = styleTagTransform;
 
 /***/ }),
 
+/***/ "./src/header.js":
+/*!***********************!*\
+  !*** ./src/header.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+function createHeader() {
+    const header = document.createElement('header');
+    document.body.appendChild(header);
+
+    header.appendChild(createTitle('THE HAPPY', 'LITTLE PIGLET'));
+    header.appendChild(createMenu('Home', 'Menu', 'Contact'));
+}
+
+function createTitle(...lines) {
+    const title = document.createElement('a');
+    title.classList.add('title');
+    title.href = '#';
+
+    for (const line of lines) {
+        const titleLine = document.createElement('h1');
+        titleLine.textContent = `${line}`;
+        title.appendChild(titleLine);
+    }
+
+    return title;
+}
+
+function createMenu(...items) {
+    const menu = document.createElement('div');
+    menu.classList.add('menu')
+
+    // Add checkbox then replace it by ☰ on small screens to create responsive menu
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'checkbox_toggle';
+    checkbox.classList.add('checkbox_toggle');
+    menu.appendChild(checkbox);
+
+    const checkboxLabel = document.createElement('label');
+    checkboxLabel.htmlFor = 'checkbox_toggle';
+    checkboxLabel.classList.add('hamburger');
+    checkboxLabel.textContent = '☰';
+    menu.appendChild(checkboxLabel);
+
+    const menuNav = document.createElement('nav');
+    menuNav.classList.add('menu');
+    menu.appendChild(menuNav);
+
+    const menuUl = document.createElement('ul');
+    menuUl.classList.add('menu');
+    menuNav.appendChild(menuUl);
+
+    for (const item of items) {
+        const itemLi = document.createElement('li');
+        menuUl.appendChild(itemLi);
+        const itemA = document.createElement('a');
+        itemA.textContent = `${item}`;
+        itemA.href = '#';
+        itemLi.appendChild(itemA);
+    }
+
+    return menu;
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createHeader);
+
+/***/ }),
+
 /***/ "./src/bg.jpg":
 /*!********************!*\
   !*** ./src/bg.jpg ***!
@@ -725,68 +798,11 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
+/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./header */ "./src/header.js");
 
 
-createHeader();
 
-function createHeader() {
-    const header = document.createElement('header');
-    document.body.appendChild(header);
-
-    header.appendChild(createTitle('THE HAPPY', 'LITTLE PIGLET'));
-    header.appendChild(createMenu('Home', 'Menu', 'Contact'));
-}
-
-function createTitle(...lines) {
-    const title = document.createElement('a');
-    title.classList.add('title');
-    title.href = '#';
-
-    for (const line of lines) {
-        const titleLine = document.createElement('h1');
-        titleLine.textContent = `${line}`;
-        title.appendChild(titleLine);
-    }
-
-    return title;
-}
-
-function createMenu(...items) {
-    const menu = document.createElement('div');
-    menu.classList.add('menu')
-
-    // Add checkbox then replace it by ☰ on small screens to create responsive menu
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.id = 'checkbox_toggle';
-    checkbox.classList.add('checkbox_toggle');
-    menu.appendChild(checkbox);
-
-    const checkboxLabel = document.createElement('label');
-    checkboxLabel.htmlFor = 'checkbox_toggle';
-    checkboxLabel.classList.add('hamburger');
-    checkboxLabel.textContent = '☰';
-    menu.appendChild(checkboxLabel);
-
-    const menuNav = document.createElement('nav');
-    menuNav.classList.add('menu');
-    menu.appendChild(menuNav);
-
-    const menuUl = document.createElement('ul');
-    menuUl.classList.add('menu');
-    menuNav.appendChild(menuUl);
-
-    for (const item of items) {
-        const itemLi = document.createElement('li');
-        menuUl.appendChild(itemLi);
-        const itemA = document.createElement('a');
-        itemA.textContent = `${item}`;
-        itemA.href = '#';
-        itemLi.appendChild(itemA);
-    }
-
-    return menu;
-}
+(0,_header__WEBPACK_IMPORTED_MODULE_1__["default"])();
 })();
 
 /******/ })()
