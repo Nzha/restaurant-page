7766a3869f4cfe19396d4e6fe14770db0fafde7a
Adrien <adrien.biz@gmail.com>
2022-10-23 14:09:10 +0400

    Add Menu title and description

--- src/menu.js
+++ src/menu.js
@@ -12,9 +12,17 @@ function loadMenu() {
     main.classList.add('main', 'menu');
     document.body.appendChild(main);
 
+    const menuHeader = document.createElement('div');
+    menuHeader.classList.add('menu-header');
+    main.appendChild(menuHeader);
+
     const title = document.createElement('h1');
     title.textContent = 'MENU';
-    main.appendChild(title);
+    menuHeader.appendChild(title);
+
+    const description = document.createElement('p');
+    description.textContent = 'A culinary tour of BBQ traditions from around this great nation and this beautiful planet. 1.8 million years ago, humans first began cooking meat with fire. For some reason, this hasn’t led to world peace, but we’re pretty sure if we fire up enough BBQ, it will.';
+    menuHeader.appendChild(description);
 }
 
 export default loadMenu;
