f4a5b778b7210f44f029623f7933bd7f360a6c30
Adrien <adrien.biz@gmail.com>
2022-10-23 20:34:05 +0400

    Add additional menu items

--- dist/main.js
+++ dist/main.js
@@ -781,86 +781,87 @@ function loadMenu() {
             'Creating the *perfect* palette of Federalist Pig flavors is an art. And we’re artists. Served with Texas toast & pickles.',
             'h2'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'TWO STEP',
             'pick any 2 meats & 2 small sides'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'RIB & CHICKEN',
             '1/2 lb. of ribs, 1/4 chicken & 2 small sides'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'SAMPLER PLATTER',
             'pick any 3 meats & 2 small sides'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'PITMASTER SPECIAL FOR 2',
             'pick any 4 meats & 3 small sides'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'FEED THE FAM',
             '1/2 lb. brisket, 1/2 lb. chopped pork, 1/2 lb. chicken, 1 lb. pork rib tips & 4 large sides'
         )
-    )
+    );
+    leftColumn.appendChild(document.createElement('hr'));
     leftColumn.appendChild(
         createMenuItem(
             'SIDES',
             'A wise leader once said that unless both sides win, no agreement can be permanent. So — just agree to get more sides later.',
             'h2'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'SMOKED CHEDDAR MAC & CHEESE',
             'shell pasta baked with smoked cheddar and jack cheese'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'CRISPY BRUSSELS SPROUTS',
             'fried and seasoned with Fed Pig BBQ rub'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'CHIPOTLE GARLIC GREEN BEANS',
             'sautéed with roasted garlic and chipotle peppers'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'RED BLISS POTATO SALAD',
             'southern style potato salad with chopped pickles, celery, and hard boiled eggs'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'CLASSIC COLESLAW',
             'carolina style chopped cabbage slaw'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'BBQ BEANS',
             'smokey, savory, and a little sweet'
         )
-    )
+    );
     leftColumn.appendChild(
         createMenuItem(
             'LOADED FRIES',
             'seasoned fries topped with pulled pork, melted cheese, ranch, and BBQ sauce'
         )
-    )
+    );
 
     // RIGHT COLUMN
     const rightColumn = document.createElement('div');
@@ -873,38 +874,71 @@ function loadMenu() {
             'When bread meets what could go between bread, there’s no end to the culinary possibilities.',
             'h2'
         )
-    )
+    );
     rightColumn.appendChild(
         createMenuItem(
             'CAROLINA ON MY MIND',
             'chopped pork shoulder, crispy skin, spicy vinegar sauce, coleslaw, sesame bun'
         )
-    )
+    );
     rightColumn.appendChild(
         createMenuItem(
             'MUSIC CITY',
             'spicy fried chicken breast, greens, pickles, buttermilk ranch, sesame bun'
         )
-    )
+    );
     rightColumn.appendChild(
         createMenuItem(
             'BIG CHEESE',
             'chopped pork or brisket, melted cheese, crispy onions, BBQ sauce, sesame bun'
         )
-    )
+    );
     rightColumn.appendChild(
         createMenuItem(
             'CLUB',
             'smoked turkey, crispy pork belly, avocado, tomato, garlic aioli, texas toast'
         )
-    )
+    );
     rightColumn.appendChild(
         createMenuItem(
             'JIVE TURKEY',
             'smoked turkey, melted cheese, grilled tomato, crispy onions, ranch, BBQ sauce, texas toast'
         )
-    )
-
+    );
+    rightColumn.appendChild(document.createElement('hr'));
+    rightColumn.appendChild(
+        createMenuItem(
+            'SALADS',
+            `Crunch crunch, who's there? You, getting a tangy, acidic crunch of greens to really make your plate sing.`,
+            'h2'
+        )
+    );
+    rightColumn.appendChild(
+        createMenuItem(
+            'BBQ GREEN SALAD',
+            'Choice of meat, mixed greens, smoked tomato, pickles, crispy onions, apple cider vinaigrette'
+        )
+    );
+    rightColumn.appendChild(document.createElement('hr'));
+    rightColumn.appendChild(
+        createMenuItem(
+            'DESSERT',
+            `The kind of blue-ribbon treats usually reserved for a State Fair judge, straight to you.`,
+            'h2'
+        )
+    );
+    rightColumn.appendChild(
+        createMenuItem(
+            `NANA'S BANANA NANA PUDDING`,
+            'fresh bananas layered with vanilla pudding and nilla wafers'
+        )
+    );
+    rightColumn.appendChild(
+        createMenuItem(
+            'OREO CHOCOLATE PUDDING',
+            'chocolate pudding layered with oreo cookie crumbles'
+        )
+    );
 }
 
 function createMenuItem(name, description, heading = 'h3') {
