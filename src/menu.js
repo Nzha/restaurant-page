c51ef021aaea079895baf905eea4147e9dd34428
Adrien <adrien.biz@gmail.com>
2022-10-23 15:06:41 +0400

    Create function to create menu items

--- src/menu.js
+++ src/menu.js
@@ -23,6 +23,55 @@ function loadMenu() {
     const description = document.createElement('p');
     description.textContent = 'A culinary tour of BBQ traditions from around this great nation and this beautiful planet. 1.8 million years ago, humans first began cooking meat with fire. For some reason, this hasn’t led to world peace, but we’re pretty sure if we fire up enough BBQ, it will.';
     menuHeader.appendChild(description);
+
+    const menu = document.createElement('div');
+    menu.classList.add('menu-content');
+    main.appendChild(menu);
+
+    // LEFT COLUMN
+    const leftColumn = document.createElement('div');
+    leftColumn.classList.add('left-column');
+    menu.appendChild(leftColumn);
+
+    // const menuCategory = document.createElement('div');
+    // menuCategory.classList.add('menu-category');
+    // leftColumn.appendChild(menuCategory);
+
+    leftColumn.appendChild(
+        createMenuItem(
+            'BRISKET',
+            'chopped or sliced'
+        )
+    )
+
+    // RIGHT COLUMN
+    const rightColumn = document.createElement('div');
+    rightColumn.classList.add('right-column');
+    menu.appendChild(rightColumn);
+
+    rightColumn.appendChild(
+        createMenuItem(
+            'CAROLINA ON MY MIND',
+            'chopped pork shoulder, crispy skin, spicy vinegar sauce, coleslaw, sesame bun'
+        )
+    )
+
+}
+
+function createMenuItem(name, description) {
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('menu-item');
+
+    const itemName = document.createElement('h2');
+    itemName.textContent = name;
+
+    const itemDescription = document.createElement('p');
+    itemDescription.textContent = description;
+
+    menuItem.appendChild(itemName);
+    menuItem.appendChild(itemDescription);
+
+    return menuItem;
 }
 
 export default loadMenu;
